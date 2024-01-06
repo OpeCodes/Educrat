4d02368aaf323a1f1482646d180e60dc08bcd05f
Adedokun peter <peteradedokun436@gmail.com>
2024-01-06 18:17:43 +0100

    sidebar layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,24 @@
-import { Flex, Image, Stack, Box, Text, Badge, Button } from "@chakra-ui/react";
+import {
+  Flex,
+  Image,
+  Stack,
+  Box,
+  Text,
+  Badge,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  useDisclosure,
+  DrawerCloseButton,
+} from "@chakra-ui/react";
 import { NavLink } from "react-router-dom";
 import logo from "../assets/logo.svg";
 import { FiSearch } from "react-icons/fi";
 import { IoCartOutline } from "react-icons/io5";
-
+import { BiMenuAltRight } from "react-icons/bi";
 const links = [
   {
     id: 1,
@@ -31,7 +46,9 @@ const links = [
     href: "/contact",
   },
 ];
+
 const Navbar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <Stack>
       <Flex
@@ -41,31 +58,31 @@ const Navbar = () => {
         justify={"space-between"}
         align={"center"}
       >
-        <Flex>
+        <Flex justify={"space-between"} align={"center"}>
           <Box width={"160px"}>
-            <Image
-              src={logo}
-              alt="Dan Abramov"
-            />
+            <Image src={logo} alt="Dan Abramov" />
           </Box>
         </Flex>
-        <Flex columnGap={4} color={"white"}>
+        <Flex
+          columnGap={4}
+          color={"white"}
+          display={{ base: "none", lg: "flex" }}
+        >
           {links.map(({ id, name, href }) => (
             <Box
               paddingX={"10px"}
               borderRadius={5}
               paddingY={"2px"}
-            //   _hover={{ background: "blue" ,}}
+              //   _hover={{ background: "blue" ,}}
               transition={"all"}
+              key={id}
             >
               <NavLink
                 to={href}
-                key={id}
-                style={({ isActive, }) => {
+                style={({ isActive }) => {
                   return {
                     fontWeight: isActive ? "bold" : "",
                     color: isActive ? "#4f547b" : "white",
-                    //   viewTransitionName: isTransitioning ? "slide" : "",
                   };
                 }}
               >
@@ -82,7 +99,6 @@ const Navbar = () => {
             <Text cursor={"pointer"}>
               <IoCartOutline fontSize={"25px"} />
             </Text>
-
             <Badge
               position="absolute"
               top="-4"
@@ -90,18 +106,29 @@ const Navbar = () => {
               borderRadius="100%"
               bg="red.500"
               color="white"
-              //   fontSize="sm"
-              //   px={1}
-              // p={1}
               textAlign={"center"}
             >
               10
             </Badge>
           </Box>
-          <Button color="#ffffff" variant="link">
+          <Box
+            fontSize={"50px"}
+            color={"white"}
+            cursor={"pointer"}
+            display={{ base: "black", lg: "none" }}
+            onClick={onOpen}
+          >
+            <BiMenuAltRight />
+          </Box>
+          <Button
+            color="#ffffff"
+            variant="link"
+            display={{ base: "none", md: "flex" }}
+          >
             Log In
           </Button>
           <Button
+            display={{ base: "none", md: "flex" }}
             px={8}
             bg="white"
             color="black"
@@ -114,6 +141,36 @@ const Navbar = () => {
           </Button>
         </Flex>
       </Flex>
+      <Drawer
+        placement="left"
+        onClose={onClose}
+        isOpen={isOpen}
+        size={{ base: "full", sm: "md" }}
+      >
+        <DrawerOverlay />
+        <Box bg="white" p={5} display={{ base: "none", md: "block" }}>
+          <DrawerCloseButton bg="white" borderRadius={"100%"} />
+        </Box>
+
+        <DrawerContent>
+          <DrawerHeader
+            borderBottomWidth="1px"
+            display={"flex"}
+            justifyContent={"space-between"}
+          >
+            <Text>Basic side</Text>
+            <Box display={{ base: "block", md: "none" }}>
+              <DrawerCloseButton bg="white" borderRadius={"100%"} />
+            </Box>
+          </DrawerHeader>
+
+          <DrawerBody>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </Stack>
   );
 };
