99e0a85ecd1b0815f2514d3478ec2f5aaddc8194
Adedokun peter <peteradedokun436@gmail.com>
2024-01-06 22:13:27 +0100

    password icon

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -6,26 +6,33 @@ import {
   FormLabel,
   Input,
   Button,
-  Flex
+  Flex,
+  InputRightElement,
+  InputGroup,
 } from "@chakra-ui/react";
 import { Formik } from "formik";
-import * as Yup from "yup";
 import { useState } from "react";
+import { IoIosEye, IoIosEyeOff } from "react-icons/io";
+
 import { SignUpSchema } from "../schemas";
 const initialValues = {
-    firstName: "peter",
-    lastName: "adedokun",
-    email: "d@gmail.com",
-    password: "Peter12",
-    confirmPassword: "Peter12",
-  };
+  firstName: "peter",
+  lastName: "adedokun",
+  email: "d@gmail.com",
+  password: "Peter12",
+  confirmPassword: "Peter12",
+};
 const SignUp = () => {
-    const [showPassword, setShowPassword] = React.useState<boolean>(false)
-    const handleClick = () => setShow(!show)
-    const handleSubmit = (values: typeof initialValues) => {
-      console.log(values);
-      console.log("sucessfull");
-    };
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [showConfirmPassword, setShowConfirmPassword] =
+    useState<boolean>(false);
+  const handlePasswordClick = () => setShowPassword(!showPassword);
+  const handleConfirmPasswordClick = () =>
+    setShowConfirmPassword(!showConfirmPassword);
+  const handleSubmit = (values: typeof initialValues) => {
+    console.log(values);
+    console.log("sucessfull");
+  };
   return (
     <Stack mt={5}>
       <Box textAlign="center">
@@ -39,11 +46,11 @@ const SignUp = () => {
         <Formik
           initialValues={initialValues}
           validationSchema={SignUpSchema}
-          onSubmit={ handleSubmit}
+          onSubmit={handleSubmit}
           //   onSubmit={handleSubmit}
         >
-          {({ handleChange,  handleSubmit, values, errors }) => (
-            <Flex  rowGap={"5px"} flexDirection="column">
+          {({ handleChange, handleSubmit, values, errors }) => (
+            <Flex rowGap={"5px"} flexDirection="column">
               <FormControl isRequired>
                 <FormLabel>First Name</FormLabel>
                 <Input
@@ -94,31 +101,55 @@ const SignUp = () => {
               </FormControl>
               <FormControl isRequired>
                 <FormLabel>Password</FormLabel>
-                <Input
-                  type="type"
-                  variant="filled"
-                  placeholder="password"
-                  value={values.password}
-                  name="password"
-                  onChange={handleChange}
-                />
-                {errors.email && (
+                <InputGroup size="md">
+                  <Input
+                    pr="4.5rem"
+                    type={showPassword ? "text" : "password"}
+                    variant="filled"
+                    placeholder="password"
+                    value={values.password}
+                    name="password"
+                    onChange={handleChange}
+                  />
+                  <InputRightElement width="4.5rem" >
+                    <Button h="1.75rem" size="sm" onClick={handlePasswordClick}  _hover={{background: "none"}}>
+                      {showPassword ?<IoIosEye fontSize="20px" /> : <IoIosEyeOff  fontSize="20px"/>}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+
+                {errors.password && (
                   <Text style={{ color: "red", marginTop: 5 }} fontSize="14px">
                     {errors.password}
                   </Text>
                 )}
               </FormControl>
+
               <FormControl isRequired>
                 <FormLabel>Confirm Password</FormLabel>
-                <Input
-                  type="text"
-                  variant="filled"
-                  placeholder="Confirm Password"
-                  value={values.confirmPassword}
-                  name="confirmPassword"
-                  onChange={handleChange}
-                />
-                {errors.email && (
+                <InputGroup size="md">
+                  <Input
+                    pr="4.5rem"
+                    type={showConfirmPassword ? "text" : "password"}
+                    variant="filled"
+                    placeholder="Confirm Password"
+                    value={values.confirmPassword}
+                    name="confirmPassword"
+                    onChange={handleChange}
+                  />
+                  <InputRightElement width="4.5rem">
+                    <Button
+                      h="1.75rem"
+                      size="md"
+                      onClick={handleConfirmPasswordClick}
+                      _hover={{background: "none"}}
+                    >
+                      {showConfirmPassword ? <IoIosEye fontSize="20px" /> : <IoIosEyeOff fontSize="20px" />}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+
+                {errors.confirmPassword && (
                   <Text style={{ color: "red", marginTop: 5 }} fontSize="14px">
                     {errors.confirmPassword}
                   </Text>
