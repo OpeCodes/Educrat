5be71799de7bc6fbbc7d70742fb2befb89a8dcc5
Adedokun peter <peteradedokun436@gmail.com>
2024-01-07 10:33:17 +0100

    category gird section layout

--- src/pages/course/Courses.tsx
+++ src/pages/course/Courses.tsx
@@ -1,4 +1,17 @@
-import { Stack, Flex, Text, Box, Grid, GridItem } from "@chakra-ui/react";
+import {
+  Stack,
+  Flex,
+  Text,
+  Box,
+  Grid,
+  GridItem,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Divider,
+} from "@chakra-ui/react";
 const Courses = () => {
   return (
     <Stack>
@@ -10,11 +23,31 @@ const Courses = () => {
           Write an introductory description of the category.
         </Text>
       </Box>
-      <Stack maxW="90%" mx="auto" w="100%">
-        <Grid h="200px" templateColumns="repeat(4, 1fr)" columnGap={10}>
-          <GridItem rowSpan={2} colSpan={1} bg="tomato" />
-          <GridItem width="100%" colSpan={3} bg="papayawhip">
-            <Flex justify={"space-between"}>
+      <Stack maxW={{base: "95%", md: "90%"}} mx="auto" w="100%">
+        <Grid templateColumns="repeat(4, 1fr)" columnGap={10}>
+          <GridItem rowSpan={2} colSpan={1}  borderWidth={0}  >
+            <Accordion defaultIndex={[0]} allowMultiple borderWidth={0} borderColor={"white"} >
+              <AccordionItem  _hover={{backgroundColor: "none"}}>
+                <h2>
+                  <AccordionButton borderColor={"white"} _hover={{backgroundColor: "none"}}>
+                    <Box as="span" flex="1" textAlign="left" fontSize={"20px"}>
+                      Category
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </AccordionPanel>
+              </AccordionItem>
+            </Accordion>
+            <Divider orientation='horizontal' mt={3}/>
+          </GridItem>
+          <GridItem width="100%" colSpan={3}  p={2}>
+            <Flex justify={"space-between"} mt={3}>
               <Text>showing 30 results</Text>
               <Flex>
                 <Text>a</Text>
