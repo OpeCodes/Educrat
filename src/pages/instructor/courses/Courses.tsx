7d2fae6f6a5dde226fab8a32019e822036b48591
Adedokun peter <peteradedokun436@gmail.com>
2024-01-15 17:34:39 +0100

    responsiveness

--- src/pages/instructor/courses/Courses.tsx
+++ src/pages/instructor/courses/Courses.tsx
@@ -6,7 +6,7 @@ import {
   Text,
   Stack,
   Select,
-  Button
+  Button,
 } from "@chakra-ui/react";
 
 import { FiSearch } from "react-icons/fi";
@@ -16,39 +16,37 @@ const Courses = () => {
       <Text fontSize={45} fontWeight={"600"}>
         Courses
       </Text>
-      <Flex justify={"space-between"} align={"center"}>
-      <Flex columnGap={7}>
-        <Stack width="450px">
-          <InputGroup>
-            <Input placeholder="Enter search for courses" variant="filled" />
-            <InputRightElement>
-              <FiSearch />
-            </InputRightElement>
-          </InputGroup>
-        </Stack>
-        <Select placeholder="Select option" variant={"filled"}>
-          <option value="option1">Newest</option>
-          <option value="option2">Oldest</option>
-          <option value="option3">Option 3</option>
-        </Select>
-      </Flex>
-      <Button
-                    bg={"#00FF84"}
-                    // isLoading={isPending}
-                    // loadingText="Loading"
-                    colorScheme="teal"
-                    variant="outline"
-                    spinnerPlacement="end"
-                    // onClick={() => handleSubmit()}
-                    mt={3}
-                    borderWidth={2}
-                    py={3}
-                    borderColor={"#00FF84"}
-                    _hover={{ background: "none", color: "#00FF84" }}
-                    size='lg'
-                  >
-                    New Course
-                  </Button>
+      <Flex justify={"space-between"} align={"center"} flexWrap={"wrap"} rowGap={0}>
+        <Flex columnGap={7} flexWrap={"wrap"} rowGap={5}>
+          <Stack width="55%">
+            <InputGroup>
+              <Input placeholder="Enter search for courses" variant="filled" />
+              <InputRightElement>
+                <FiSearch />
+              </InputRightElement>
+            </InputGroup>
+          </Stack>
+          <Select placeholder="Select option" variant={"filled"} width={"35%"}>
+            <option value="option1">Newest</option>
+            <option value="option2">Oldest</option>
+            <option value="option3">Option 3</option>
+          </Select>
+        </Flex>
+        <Button
+          bg={"#00FF84"}
+          colorScheme="teal"
+          variant="outline"
+          spinnerPlacement="end"
+          // onClick={() => handleSubmit()}
+          mt={3}
+          borderWidth={2}
+          py={3}
+          borderColor={"#00FF84"}
+          _hover={{ background: "none", color: "#00FF84" }}
+          size="lg"
+        >
+          New Course
+        </Button>
       </Flex>
     </Stack>
   );
