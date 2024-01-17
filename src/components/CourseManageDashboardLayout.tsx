dd7e9abc9d2e8266bbdb8c082d7832ec10903055
Adedokun peter <peteradedokun436@gmail.com>
2024-01-17 23:46:08 +0100

    reponsiveness

--- src/components/CourseManageDashboardLayout.tsx
+++ src/components/CourseManageDashboardLayout.tsx
@@ -6,15 +6,22 @@ const CourseManageDashboardLayout = () => {
   return (
     <>
       <CourseManageNavbar />
+        
       <Flex flexWrap={"wrap"}>
-        <Stack mr={20} m={20}>
+        <Stack 
+        // mr={20} m={20}
+        ml={{base: 5,lg: 20}}
+        mr={{base: 5, lg: 20}}
+        mt={20}
+        >
           <CourseManageSidebar />
         </Stack>
         <Stack
           bg="white"
-          maxW={"70%"}
+          maxW={{base: "100%", md: "70%"}}
           my={10}
           w="full"
+          mr={{base: 0, lg: 5}}
           boxShadow="0 0.75rem 1rem rgb(189 197 209 / 90%)"
         >
           <Outlet />
