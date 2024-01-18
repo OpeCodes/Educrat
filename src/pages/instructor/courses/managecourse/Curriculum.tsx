532995c404111ec5cf88667802c29cda0bd7953f
Adedokun peter <peteradedokun436@gmail.com>
2024-01-18 23:27:10 +0100

    curriculum text section

--- src/pages/instructor/courses/managecourse/Curriculum.tsx
+++ src/pages/instructor/courses/managecourse/Curriculum.tsx
@@ -7,6 +7,15 @@ const Curriculum = () => {
         Curriculum
       </Text>
       <Divider />
+      <Text p={5} fontSize={14}>
+        Start putting together your course by creating sections, lectures and
+        practice (quizzes, coding exercises and assignments). Start putting
+        together your course by creating sections, lectures and practice
+        activities (quizzes, coding exercises and assignments). Use your course
+        outline to structure your content and label your sections and lectures
+        clearly. If you’re intending to offer your course for free, the total
+        length of video content must be less than 2 hours.
+      </Text>
     </Stack>
   );
 };
