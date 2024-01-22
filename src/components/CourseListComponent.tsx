eb755534f715d12496196b307398d8bc7100aeb3
Adedokun peter <peteradedokun436@gmail.com>
2024-01-22 22:16:38 +0100

    hover

--- src/components/CourseListComponent.tsx
+++ src/components/CourseListComponent.tsx
@@ -1,6 +1,14 @@
 // ListWithButtons.tsx
 import { useState } from "react";
-import { Box, Flex, Image, Stack, Text, Progress } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Image,
+  Stack,
+  Text,
+  Progress,
+  Button,
+} from "@chakra-ui/react";
 import { FaCircleExclamation } from "react-icons/fa6";
 
 // interface ListItem {
@@ -53,26 +61,77 @@ const CourseListComponent = () => {
     //   ))}
     // </VStack>
     <>
-      <Flex borderColor="gray" borderWidth="1px" justify={"space-between"} columnGap={5} height="130px">
-        <Flex columnGap={4} w={{base: "100%", md: "40%"}} >
-          <Image
-            src="https://bit.ly/dan-abramov"
-            alt="Dan Abramov"
-            w="120px"
-            h="full"
-          />
-          <Flex flexDirection={"column"} justify={"space-between"} my={2} width="70%"  >
-            <Text fontWeight={"bold"}>Learn figma from peterdd</Text>
-            <Text fontWeight={"700"}>DRAFT</Text>
+      {items.map((item) => (
+        <Stack
+          position={"relative"}
+          onMouseEnter={() => handleMouseEnter(item.id)}
+          onMouseLeave={handleMouseLeave}
+        >
+          <Flex
+            borderColor="gray"
+            borderWidth="1px"
+            justify={"space-between"}
+            columnGap={5}
+            mb={3}
+            height="130px"
+          >
+            <Flex columnGap={4} w={{ base: "100%", md: "40%" }}>
+              <Image
+                src="https://bit.ly/dan-abramov"
+                alt="Dan Abramov"
+                w="120px"
+                h="full"
+              />
+              <Flex
+                flexDirection={"column"}
+                justify={"space-between"}
+                my={2}
+                width="70%"
+              >
+                <Text fontWeight={"bold"}>Learn figma from peterdd</Text>
+                <Text fontWeight={"700"}>DRAFT</Text>
+              </Flex>
+            </Flex>
+            <Flex
+              display={{ base: "none", md: "flex" }}
+              ml={5}
+              w="60%"
+              pl={2}
+              align={"center"}
+              mr={5}
+              columnGap={5}
+            >
+              <Text fontWeight={"bold"}>Finish your course</Text>
+              <Box maxWidth={"75%"} w="100%">
+                <Progress value={20} />
+              </Box>
+            </Flex>
           </Flex>
-        </Flex>
-        <Flex display={{base: "none", md: "flex"}} ml={5} w="60%" pl={2} align={"center"} mr={5} columnGap={5}>
-          <Text fontWeight={"bold"}>Finish your course</Text>
-          <Box maxWidth={"75%"} w="100%">
-          <Progress value={20} />
-          </Box>
-        </Flex>
-      </Flex>
+          {hoveredItem === item.id && (
+            <Box width={"100%"} backgroundColor={"black"}>
+              <Text
+                size="sm"
+                // opacity={0.3}
+                // background
+                width="100%"
+                h="100%"
+                position="absolute"
+                top={0}
+                right={0}
+                zIndex={1}
+                fontSize={"20px"}
+                bg="blue"
+                display="flex"
+                justifyContent={"center"}
+                alignItems={"center"}
+                color={"white"}
+              >
+                Edit/Manage Course
+              </Text>
+            </Box>
+          )}
+        </Stack>
+      ))}
     </>
   );
 };
--- src/pages/instructor/courses/Courses.tsx
+++ src/pages/instructor/courses/Courses.tsx
@@ -14,8 +14,8 @@ import { Link } from "react-router-dom";
 import { useGetAllUserCourse } from "../../../hooks/course";
 import { CourseListComponent } from "../../../components";
 const Courses = () => {
-  const {data,isError,isPending} = useGetAllUserCourse()
-  console.log(data)
+  // const {data,isError,isPending} = useGetAllUserCourse()
+  // console.log(data)
 
 
   
