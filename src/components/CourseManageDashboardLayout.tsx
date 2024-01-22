9486f07bc88ce5baff0c0970a43e467c33b99018
Adedokun peter <peteradedokun436@gmail.com>
2024-01-22 06:49:13 +0100

    bug fixes

--- src/components/CourseManageDashboardLayout.tsx
+++ src/components/CourseManageDashboardLayout.tsx
@@ -1,19 +1,19 @@
-import { Outlet, useParams } from "react-router-dom";
+import { Outlet} from "react-router-dom";
 import { CourseManageNavbar, CourseManageSidebar } from ".";
 import { Flex, Stack } from "@chakra-ui/react";
-import { useGetSingleCourse } from "../hooks/course";
-import { useEffect } from "react";
-import { Error } from "../pages/auth";
+// import { useGetSingleCourse } from "../hooks/course";
+// import { useEffect } from "react";
+// import { Error } from "../pages/auth";
 
 const CourseManageDashboardLayout = () => {
-  const { id } = useParams();
-  const { getSingleCourse, isError } = useGetSingleCourse();
+  // const { id } = useParams();
+  // const { getSingleCourse, isError } = useGetSingleCourse();
   // useEffect(() => {
   //   getSingleCourse({ course: id });
   // }, []);
-  if (isError) {
-    return <Error />;
-  }
+  // if (isError) {
+  //   return <Error />;
+  // }
   return (
     <>
       <CourseManageNavbar />
