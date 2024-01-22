a7e15a7e2f65881f22c548958ecf98640cb93ed7
Adedokun peter <peteradedokun436@gmail.com>
2024-01-22 17:53:59 +0100

    error page

--- src/pages/auth/Error.tsx
+++ src/pages/auth/Error.tsx
@@ -1,8 +1,22 @@
-
+import {Link ,useRouteError} from "react-router-dom"
 const Error = () => {
-  return (
-    <div>Error</div>
-  )
+  const error: any  = useRouteError()
+ console.log(error)
+  if (error.status === 404) {
+    return (
+        <div>
+          {/* <img src={img} alt="not found" /> */}
+          <h3>Ohh! page not found</h3>
+          <p>We can't seem to find the page you're looking for</p>
+          <Link to="/">back home</Link>
+        </div>
+    );
+  }
+   return (
+       <div>
+         <h3>something went wrong contact adedokunpeter11@gmail.com</h3>
+       </div>
+   );
 }
 
 export default Error
