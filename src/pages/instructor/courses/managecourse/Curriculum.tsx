918557df413584b59b2d0179f0a4ed4ab7002849
Adedokun peter <peteradedokun436@gmail.com>
2024-03-26 19:34:16 +0100

    lecture description layout

--- src/pages/instructor/courses/managecourse/Curriculum.tsx
+++ src/pages/instructor/courses/managecourse/Curriculum.tsx
@@ -1040,9 +1040,73 @@ const Curriculum = () => {
                                           {/* ************************description container************************** */}
                                           {isOpendescription[id] && (
                                             <Stack>
-                                              <Text fontWeight={"bold"}>
-                                                Lecture Description
-                                              </Text>
+                                               <Text fontWeight={"bold"}>Text</Text>
+                                    <Formik
+                                      initialValues={articleEditInitialValue}
+                                      validationSchema={
+                                        ArticleCreateLectureSchema
+                                      }
+                                      onSubmit={(values: any) => {
+                                        editArticleLectureCourse({
+                                          articleId: content?.id,
+                                          user: values,
+                                        });
+                                        setTimeout(() => {
+                                          toggleIsOpenEditArticle(id);
+                                        }, 2000);
+                                      }}
+                                    >
+                                      {({
+                                        handleChange,
+                                        handleSubmit: handleEditArticleSubmit,
+                                        values,
+                                        errors,
+                                      }) => (
+                                        <Stack>
+                                          <ReactQuill
+                                            theme="snow"
+                                            value={values.body}
+                                            onChange={handleChange("body")}
+                                          />
+                                          {errors?.body && (
+                                            <Text
+                                              style={{
+                                                color: "red",
+                                                marginTop: 5,
+                                              }}
+                                              fontSize="14px"
+                                            >
+                                              enter title
+                                            </Text>
+                                          )}
+
+                                          <Flex
+                                            justify={"end"}
+                                            align={"center"}
+                                            columnGap={5}
+                                            mt={"2.9rem"}
+                                          >
+                                            <Button
+                                              color="#ffffff"
+                                              fontWeight={"500"}
+                                              fontSize={14}
+                                              as={"button"}
+                                              py={2}
+                                              px={4}
+                                              variant="outline"
+                                              spinnerPlacement="end"
+                                              onClick={() =>
+                                                handleEditArticleSubmit()
+                                              }
+                                              type="button"
+                                              backgroundColor={"black"}
+                                            >
+                                              Save
+                                            </Button>
+                                          </Flex>
+                                        </Stack>
+                                      )}
+                                    </Formik>
                                               <Text
                                                 onClick={() =>
                                                   toggleIsOpenDescription(id)
@@ -1169,7 +1233,6 @@ const Curriculum = () => {
                                           as={"button"}
                                           fontWeight="bold"
                                           onClick={() => {
-                                            // toggleIsOpenInnerdescripRes(id);
                                             toggleIsOpenEditArticle(id);
                                           }}
                                         >
@@ -1238,7 +1301,7 @@ const Curriculum = () => {
                                               type="button"
                                               backgroundColor={"black"}
                                             >
-                                              Save
+                                              Saves
                                             </Button>
                                           </Flex>
                                         </Stack>
