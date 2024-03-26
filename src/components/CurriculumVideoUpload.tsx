a8441caa205c1cd413fb8ac3a636d2ece548e405
Adedokun peter <peteradedokun436@gmail.com>
2024-03-26 15:34:38 +0100

    clean up

--- src/components/CurriculumVideoUpload.tsx
+++ src/components/CurriculumVideoUpload.tsx
@@ -36,7 +36,7 @@ const CurriculumVideoUpload: React.FC<ImageUploadProps> = ({
   const { id: ID } = useParams();
   const { getSingleCourse } = useGetSingleCourse(ID);
   const { deleteVideoLecture } = useDeleteVideoLecture();
-  const { refetch } = useGetModuleCourse(getSingleCourse?.id);
+  const { refetch , toggleContentType2} = useGetModuleCourse(getSingleCourse?.id);
 
   const [selectedImageName, setSelectImageName] = useState<any>(null);
   const [uploadProgress, setUploadProgress] = useState<number>(0);
@@ -223,6 +223,7 @@ const CurriculumVideoUpload: React.FC<ImageUploadProps> = ({
                       refetch();
                       setSucess(false);
                       deleteVideoLecture({ videoId: contentId });
+
                     }}
                   >
                     Replace
