b27b8e2605f82f68b39ac439f6d7e171917048b6
Adedokun peter <peteradedokun436@gmail.com>
2024-03-26 17:12:26 +0100

    added video edit func

--- src/pages/instructor/courses/managecourse/Curriculum.tsx
+++ src/pages/instructor/courses/managecourse/Curriculum.tsx
@@ -1292,73 +1292,47 @@ const Curriculum = () => {
                                         </Text>
                                       </Flex>
                                     </Flex>
-                                    <Text fontWeight={"bold"}>Text</Text>
-                                    <Formik
-                                      initialValues={articleEditInitialValue}
-                                      validationSchema={
-                                        ArticleCreateLectureSchema
-                                      }
-                                      onSubmit={(values: any) => {
-                                        editArticleLectureCourse({
-                                          articleId: content?.id,
-                                          user: values,
-                                        });
-                                        setTimeout(() => {
-                                          toggleIsOpenEditArticle(id);
-                                        }, 2000);
-                                      }}
-                                    >
-                                      {({
-                                        handleChange,
-                                        handleSubmit: handleEditArticleSubmit,
-                                        values,
-                                        errors,
-                                      }) => (
-                                        <Stack>
-                                          <ReactQuill
-                                            theme="snow"
-                                            value={values.body}
-                                            onChange={handleChange("body")}
-                                          />
-                                          {errors?.body && (
-                                            <Text
-                                              style={{
-                                                color: "red",
-                                                marginTop: 5,
-                                              }}
-                                              fontSize="14px"
-                                            >
-                                              enter title
-                                            </Text>
-                                          )}
-
-                                          <Flex
-                                            justify={"end"}
-                                            align={"center"}
-                                            columnGap={5}
-                                            mt={"2.9rem"}
+                                          <Stack mx={3}>
+                                      <Tabs>
+                                        <TabList>
+                                          <Tab
+                                            fontWeight={"bold"}
+                                            color="black"
                                           >
-                                            <Button
-                                              color="#ffffff"
-                                              fontWeight={"500"}
-                                              fontSize={14}
-                                              as={"button"}
-                                              py={2}
-                                              px={4}
-                                              variant="outline"
-                                              spinnerPlacement="end"
-                                              onClick={() =>
-                                                handleEditArticleSubmit()
-                                              }
-                                              type="button"
-                                              backgroundColor={"black"}
-                                            >
-                                              Save
-                                            </Button>
-                                          </Flex>
-                                        </Stack>
-                                      )}
-                                    </Formik>
+                                            Upload Video
+                                          </Tab>
+                                          <Tab
+                                            fontWeight={"bold"}
+                                            color="black"
+                                          >
+                                            Add from library
+                                          </Tab>
+                                        </TabList>
+                                        <TabPanels>
+                                          <TabPanel>
+                                            <Stack>
+                                              <CurriculumVideoUpload
+                                                onImageUpload={
+                                                  handleUploadSuccess
+                                                }
+                                                id={id}
+                                                contentId={content?.id}
+                                              />
+                                            </Stack>
+                                          </TabPanel>
+                                          <TabPanel>
+                                            <Stack>
+                                              <Text
+                                                fontSize={16}
+                                                fontWeight={"bold"}
+                                              >
+                                                Feature incoming soon.......
+                                              </Text>
+                                            </Stack>
+                                          </TabPanel>
+                                        </TabPanels>
+                                      </Tabs>
+                                    </Stack>
                                   </Stack>
                                 )}
                             </Stack>
