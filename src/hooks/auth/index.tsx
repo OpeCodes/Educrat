2e17180a267240821b5f3c44aef3b1df3d1c1a9c
Adedokun peter <peteradedokun436@gmail.com>
2024-03-30 12:27:13 +0100

    useRegisterUser endpoint user

--- src/hooks/auth/index.tsx
+++ src/hooks/auth/index.tsx
@@ -72,12 +72,28 @@ export const useRegisterUser = () => {
       navigate("/sign-in");
     },
     onError: (error: any) => {
-      toast({
-        title: `${error.response.data.error}`,
-        status: "error",
-        duration: 5000,
-        isClosable: true,
-      });
+      if (error.response) {
+        toast({
+          title: `${error.response.data.error}`,
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      } else if (error.request) {
+        toast({
+          title: "Network error occurred. Please try again later.",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      } else {
+        toast({
+          title: "An error occurred. Please try again later.",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      }
     },
   });
   return { isPending, registerUser };
