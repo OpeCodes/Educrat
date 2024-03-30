c6274cbf3668d936add5709cc75e32889326cea8
Adedokun peter <peteradedokun436@gmail.com>
2024-03-30 16:58:01 +0100

    pricing header setup

--- src/pages/instructor/courses/managecourse/Pricing.tsx
+++ src/pages/instructor/courses/managecourse/Pricing.tsx
@@ -1,8 +1,22 @@
+import { Divider, Stack, Text } from "@chakra-ui/react";
 
 const Pricing = () => {
   return (
-    <div>Pricing</div>
-  )
-}
+    <Stack>
+      <Text p={5} fontSize={20} fontWeight={"bold"}>
+        Pricing
+      </Text>
+      <Divider />
+      <Stack p={5}>
+      <Text fontWeight={"bold"}>Set a price for your course</Text>
+      <Text fontSize={14}>
+        Please select the currency and the price tier for your course. If you’d
+        like to offer your course for free, it must have a total video length of
+        less than 2 hours. Also, courses with practice tests can not be free.
+      </Text>
+      </Stack>
+    </Stack>
+  );
+};
 
-export default Pricing
+export default Pricing;
