fbe7c2e3f2f09fe9ac2e189672ef4ac2f3216819
Adedokun peter <peteradedokun436@gmail.com>
2024-03-30 12:28:39 +0100

    useForgotPassword endpoint update

--- src/hooks/auth/index.tsx
+++ src/hooks/auth/index.tsx
@@ -163,12 +163,28 @@ export const useForgotPassword = () => {
     },
 
     onError: (error: any) => {
-      toast({
-        title: `${error.response.data.error}`,
-        status: "error",
-        duration: 5000,
-        isClosable: true,
-      });
+      if (error.response) {
+        toast({
+          title: `${error.response.data.error}`,
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      } else if (error.request) {
+        toast({
+          title: "Network error occurred. Please try again later.",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      } else {
+        toast({
+          title: "An error occurred. Please try again later.",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      }
     },
   });
   return { forgotPassword, isPending };
