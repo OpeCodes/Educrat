0b153aaed0e28781180ab5390e549c624fdb59e0
Adedokun peter <peteradedokun436@gmail.com>
2024-03-30 12:16:00 +0100

    useDeleteModuleCourse endpoint update

--- src/hooks/module/index.tsx
+++ src/hooks/module/index.tsx
@@ -103,12 +103,30 @@ export const useDeleteModuleCourse = () => {
       });
     },
     onError: (error: any) => {
-      toast({
-        title: `${error.response.data.error}`,
-        status: "error",
-        duration: 5000,
-        isClosable: true,
-      });
+     
+
+ if (error.response) {
+  toast({
+    title: `${error.response.data.error}`,
+    status: "error",
+    duration: 5000,
+    isClosable: true,
+  });
+} else if (error.request) {
+  toast({
+    title: "Network error occurred. Please try again later.",
+    status: "error",
+    duration: 5000,
+    isClosable: true,
+  });
+} else {
+  toast({
+    title: "An error occurred. Please try again later.",
+    status: "error",
+    duration: 5000,
+    isClosable: true,
+  });
+}
     },
   });
   return { deleteModule, deleteModuleLoading, };
@@ -653,12 +671,30 @@ export const useDeleteVideoLecture = () => {
       });
     },
     onError: (error: any) => {
-      toast({
-        title: `${error.response.data.error}`,
-        status: "error",
-        duration: 5000,
-        isClosable: true,
-      });
+     
+
+ if (error.response) {
+  toast({
+    title: `${error.response.data.error}`,
+    status: "error",
+    duration: 5000,
+    isClosable: true,
+  });
+} else if (error.request) {
+  toast({
+    title: "Network error occurred. Please try again later.",
+    status: "error",
+    duration: 5000,
+    isClosable: true,
+  });
+} else {
+  toast({
+    title: "An error occurred. Please try again later.",
+    status: "error",
+    duration: 5000,
+    isClosable: true,
+  });
+}
     },
   });
   return { deleteVideoLecture, deleteVideoLectureLoding};
