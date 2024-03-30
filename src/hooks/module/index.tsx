1e84060c597028a1849e0f3ba35adf85da55cd05
Adedokun peter <peteradedokun436@gmail.com>
2024-03-30 12:17:18 +0100

    useModuleCreateCourse endpoint update

--- src/hooks/module/index.tsx
+++ src/hooks/module/index.tsx
@@ -29,12 +29,30 @@ export const useModuleCreateCourse = () => {
       });
     },
     onError: (error: any) => {
-      toast({
-        title: `${error.response.data.error}`,
-        status: "error",
-        duration: 5000,
-        isClosable: true,
-      });
+    
+
+ if (error.response) {
+  toast({
+    title: `${error.response.data.error}`,
+    status: "error",
+    duration: 5000,
+    isClosable: true,
+  });
+} else if (error.request) {
+  toast({
+    title: "Network error occurred. Please try again later.",
+    status: "error",
+    duration: 5000,
+    isClosable: true,
+  });
+} else {
+  toast({
+    title: "An error occurred. Please try again later.",
+    status: "error",
+    duration: 5000,
+    isClosable: true,
+  });
+}
     },
   });
   return {
