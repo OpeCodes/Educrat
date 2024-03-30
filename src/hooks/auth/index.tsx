07818131ad836354c068ff102173c5a3f6d11555
Adedokun peter <peteradedokun436@gmail.com>
2024-03-30 12:29:09 +0100

    useVerifyAccount endpoint update

--- src/hooks/auth/index.tsx
+++ src/hooks/auth/index.tsx
@@ -209,12 +209,28 @@ export const useVerifyAccount = () => {
       }, 3000);
     },
     onError: (error: any) => {
-      toast({
-        title: `${error.response.data.error}`,
-        status: "error",
-        duration: 5000,
-        isClosable: true,
-      });
+      if (error.response) {
+        toast({
+          title: `${error.response.data.error}`,
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      } else if (error.request) {
+        toast({
+          title: "Network error occurred. Please try again later.",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      } else {
+        toast({
+          title: "An error occurred. Please try again later.",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      }
     },
   });
   return { verifyAccount };
