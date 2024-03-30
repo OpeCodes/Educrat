e35e49b5978ca5d08bbab984a3b827b8ddb6e82b
Adedokun peter <peteradedokun436@gmail.com>
2024-03-30 12:08:24 +0100

    edit module lecture course endpoint update

--- src/hooks/module/index.tsx
+++ src/hooks/module/index.tsx
@@ -407,12 +407,28 @@ export const useEditModuleLectureCourse = () => {
       });
     },
     onError: (error: any) => {
-      toast({
-        title: `${error.response.data.error}`,
-        status: "error",
-        duration: 5000,
-        isClosable: true,
-      });
+      if (error.response) {
+        toast({
+          title: `${error.response.data.error}`,
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      } else if (error.request) {
+        toast({
+          title: "Network error occurred. Please try again later.",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      } else {
+        toast({
+          title: "An error occurred. Please try again later.",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      }
     },
   });
   return { moduleEditLectureCourse, moduleEditLectureLoading };
