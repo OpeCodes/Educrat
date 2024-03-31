88f81f00fec96ab3003093834c7e8c6e11f8bb63
Adedokun peter <peteradedokun436@gmail.com>
2024-03-31 22:56:44 +0100

    course message heading

--- src/pages/instructor/courses/managecourse/CourseMessage.tsx
+++ src/pages/instructor/courses/managecourse/CourseMessage.tsx
@@ -1,8 +1,20 @@
+import { Divider, Stack, Text } from "@chakra-ui/react";
 
 const CourseMessage = () => {
   return (
-    <div>CourseMessage</div>
-  )
-}
+    <Stack>
+      <Text p={5} fontSize={20} fontWeight={"bold"}>
+        Course Messages
+      </Text>
+      <Divider />
+      <Text p={5} fontSize={14}>
+        Write messages to your students (optional) that will be sent
+        automatically when they join or complete your course to encourage
+        students to engage with course content. If you do not wish to send a
+        welcome or congratulations message, leave the text box blank.
+      </Text>
+    </Stack>
+  );
+};
 
-export default CourseMessage
+export default CourseMessage;
