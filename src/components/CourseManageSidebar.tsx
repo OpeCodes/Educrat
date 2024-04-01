f1fcfeeb8b3bbcf7bc2f05f0d77a6b7fab83f8d2
Adedokun peter <peteradedokun436@gmail.com>
2024-04-01 08:58:29 +0100

    sidebar ui update

--- src/components/CourseManageSidebar.tsx
+++ src/components/CourseManageSidebar.tsx
@@ -13,80 +13,36 @@ const CourseManageSidebar = () => {
   const links = [
     {
       id: 1,
-      name: "intended Learners",
-      href: "/",
-    },
-    {
-      id: 2,
-      name: "course structure",
-      href: "/basics",
-    },
-    {
-      id: 3,
-      name: "Setup & test video",
-      href: "/",
-    },
-    {
-      id: 4,
-      name: "Film and Edit",
-      href: "/",
-    },
-    {
-      id: 5,
       name: "Curriculum",
       href: `/instructor/courses/${getSingleCourse?.id}/manage/curriculum`,
     },
+
     {
-      id: 6,
-      name: "Captions(optional)",
-      href: "/",
-    },
-    {
-      id: 7,
-      name: "Accessbility(optional)",
-      href: "",
-    },
-    {
-      id: 8,
+      id: 2,
       name: "Course Landing Page",
       href: `/instructor/courses/${getSingleCourse?.id}/manage/basics`,
     },
     {
-      id: 9,
+      id: 3,
       name: "pricing",
       href: `/instructor/courses/${getSingleCourse?.id}/manage/pricing`,
     },
     {
-      id: 10,
+      id: 4,
       name: "Course Messages",
       href: `/instructor/courses/${getSingleCourse?.id}/manage/communications/messages`,
+    },
+    {
+      id: 5,
+      name: "Settings",
+      href: `/instructor/courses/${getSingleCourse?.id}/manage/settings`,
     },
   ];
   return (
     <Stack>
       <Stack>
-        <Text fontWeight={"bold"}>Plan your course</Text>
-        {links.slice(0, 3).map((link, i) => (
-          <Box key={i}>
-            <CourseManageNavItem to={link.href} key={i}>
-              {link.name}
-            </CourseManageNavItem>
-          </Box>
-        ))}
-      </Stack>
-      <Stack>
-        <Text fontWeight={"bold"}>Create your content</Text>
-        {links.slice(3, 7).map((link, i) => (
-          <Box key={i}>
-            <CourseManageNavItem to={link.href} key={i}>
-              {link.name}
-            </CourseManageNavItem>
-          </Box>
-        ))}
-      </Stack>
-      <Stack>
         <Text fontWeight={"bold"}>Publish your course</Text>
-        {links.slice(7, 11).map((link, i) => (
+        {links.map((link, i) => (
           <Box key={i}>
             <CourseManageNavItem to={link.href} key={i}>
               {link.name}
