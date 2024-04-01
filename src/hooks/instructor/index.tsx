eba793db6c64a8ade5449940b86c65b5f9109874
Adedokun peter <peteradedokun436@gmail.com>
2024-04-01 15:54:32 +0100

    get all instructor course enpoint

---
+++ src/hooks/instructor/index.tsx
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import customFetch from "../../utils/axios";
+
+export const useGetAllInstructorCourses = (id: any) => {
+    const {
+      data: getLectureModuleCourse,
+      isPending,
+      isSuccess,
+    } = useQuery({
+      queryKey: ["getAllInstructorCourses", id],
+      queryFn: async ({ queryKey }) => {
+        const [, id] = queryKey; // Destructure the queryKey to get the 'id'
+        const { data } = await customFetch.get(`course/instructor/${id}`);
+        return data;
+      },
+     
+    });
+  
+    return {
+      getLectureModuleCourse,
+      isPending,
+      isSuccess,
+    };
+  };
