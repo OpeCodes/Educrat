d39076ca191c1103a829387621cae8e96774489e
Adedokun peter <peteradedokun436@gmail.com>
2024-04-03 00:29:45 +0100

    ui update

--- src/pages/studentCourse/SingleEnrolledCourse.tsx
+++ src/pages/studentCourse/SingleEnrolledCourse.tsx
@@ -14,10 +14,11 @@ import {
 import logo from "../../assets/logo-2.svg";
 import { Link } from "react-router-dom";
 import { RiPlayCircleFill } from "react-icons/ri";
+const dummyData = [1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3];
 const SingleEnrolledCourse = () => {
   return (
     <Stack>
-      <Flex justify="space-between" align={"center"} p={4} bg="red">
+      <Flex justify="space-between" width={"100%"} align={"center"} p={4} bg="red" position="fixed" right="0" top="0">
         <Box width={"160px"} as={Link} to={"/"}>
           <Image src={logo} alt="logo" />
         </Box>
@@ -28,61 +29,115 @@ const SingleEnrolledCourse = () => {
         <Text>Back to courses</Text>
       </Flex>
       <Stack maxW={"90%"} width={"100%"} mx={"auto"} mt={10}>
-        <Flex justify={"space-between"}>
-          <Flex>video content here</Flex>
-          <Stack width={"30%"}>
-            <Accordion allowToggle>
-              <AccordionItem
-                style={{ borderWidth: 1, borderRadius: 15 }}
-                mb={4}
-                rowGap={6}
-              >
-                <Stack>
-                  <AccordionButton
-                    _hover={{ backgroundColor: "none" }}
-                    py={3}
-                    borderRadius={15}
-                    backgroundColor={"#F7F8FB"}
-                  >
-                    <Flex
-                      width={"100%"}
-                      justify={"space-between"}
-                      align={"center"}
-                    >
-                      <Stack>
-                        <Flex columnGap={2} fontWeight={"bold"}>
-                          <Text>Section 1:</Text>
-                          <Text>Introduction</Text>
-                        </Flex>
-                        <Flex>
-                          <Text fontSize={14}>5 / 6 | 6 mins</Text>
-                        </Flex>
-                      </Stack>
-                      <Text>
-                        <AccordionIcon fontSize={23} />
-                      </Text>
-                    </Flex>
-                  </AccordionButton>
-                </Stack>
+        <Flex justify={"space-between"}  mt={"3.5rem"}>
+          
+        <Stack>
+          <Text>peter</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk</Text>
+          <Text>ads;flkjsd;lfjk </Text>
+        </Stack>
+          
+          <Stack width={"30%"}  position="fixed" right="12" top="125px">
+          <Text fontWeight={"bold"}>Course content</Text>
 
-                <AccordionPanel>
-                  <Flex columnGap={3} align={"start"}>
-                    <Checkbox
-                      mt={1}
-                      iconColor={"black"}
-                      size="lg"
-                      borderColor={"black"}
-                      colorScheme={"blackAlpha"}
-                    />
+            <Accordion allowMultiple allowToggle maxH={"500px"} overflowY={"scroll"}>
+              {dummyData.map((_, index) => {
+                return (
+                  <AccordionItem
+                    style={{ borderWidth: 1, borderRadius: 15 }}
+                    mb={4}
+                    rowGap={6}
+
+                  >
                     <Stack>
-                      <Text>1. What is NodeJs</Text>
-                      <Flex align={"center"} color={"gray"}>
-                        <RiPlayCircleFill size={25} />1 min
-                      </Flex>
+                      <AccordionButton
+                        _hover={{ backgroundColor: "none" }}
+                        py={3}
+                        borderRadius={15}
+                        backgroundColor={"#F7F8FB"}
+                      >
+                        <Flex
+                          width={"100%"}
+                          justify={"space-between"}
+                          align={"center"}
+                        >
+                          <Stack>
+                            <Flex columnGap={2} fontWeight={"bold"}>
+                              <Text>Section {index + 1}:</Text>
+                              <Text>Introduction</Text>
+                            </Flex>
+                            <Flex>
+                              <Text fontSize={14}>5 / 6 | 6 mins</Text>
+                            </Flex>
+                          </Stack>
+                          <Text>
+                            <AccordionIcon fontSize={23} />
+                          </Text>
+                        </Flex>
+                      </AccordionButton>
                     </Stack>
-                  </Flex>
-                </AccordionPanel>
-              </AccordionItem>
+
+                    <AccordionPanel>
+                      <Flex columnGap={3} align={"start"}>
+                        <Checkbox
+                          mt={1}
+                          iconColor={"black"}
+                          size="lg"
+                          borderColor={"black"}
+                          colorScheme={"blackAlpha"}
+                        />
+                        <Stack>
+                          <Text>1. What is NodeJs</Text>
+                          <Flex align={"center"} color={"gray"}>
+                            <RiPlayCircleFill size={25} />1 min
+                          </Flex>
+                        </Stack>
+                      </Flex>
+                    </AccordionPanel>
+                  </AccordionItem>
+                );
+              })}
             </Accordion>
           </Stack>
         </Flex>
