17c214346e813bcc439b62d496d696e906e8c737
Adedokun peter <peteradedokun436@gmail.com>
2024-04-02 19:56:10 +0100

    clean up unsed code

--- src/hooks/studentCourse/index.tsx
+++ src/hooks/studentCourse/index.tsx
@@ -1,17 +1,5 @@
 import {  useQuery } from "@tanstack/react-query";
 import customFetch from "../../utils/axios";
-
-// export const useGetAllCourse = () => {
-//     const { data, isPending, isError } = useQuery({
-//       queryKey: ["allStudentCourse"],
-//       queryFn: async () => {
-//         const { data } = await customFetch.get("/course");
-//         return data;
-//       },
-//     });
-//     return { data, isPending, isError };
-//   };
-
 export const useGetStudentSingleCourse = (slug: any) => {
     const {
       data: getStudentSingleCourse,
