8f06f9db7f747146a9399b615ca4c78a348b373f
Adedokun peter <peteradedokun436@gmail.com>
2024-04-06 10:17:52 +0100

    handle refresh token

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,15 +1,58 @@
 import axios from "axios";
-import { getUserLocalStorage } from "../store/localStorage";
+import { getUserLocalStorage, removeUserFromLocalStorage} from "../store/localStorage";
+import { useNavigate } from "react-router-dom"; // Assuming you're using React Router for navigation
 
- const customFetch = axios.create({
-    baseURL: "https://educrat-be.onrender.com/api/v1"
-})
+const customFetch = axios.create({
+  baseURL: "https://educrat-be.onrender.com/api/v1"
+});
 
-customFetch.interceptors.request.use((config)=>{
-    const user = getUserLocalStorage();
-    if(user){
-        config.headers["Authorization"] = `Bearer ${user.accessToken}`
+customFetch.interceptors.request.use((config) => {
+  
+  const user = getUserLocalStorage();
+  if (user) {
+    config.headers["Authorization"] = `Bearer ${user.accessToken}`;
+  }
+  return config;
+});
+
+customFetch.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    // const originalRequest = error.config;
+    const navigate = useNavigate()
+    const user = getUserLocalStorage();  
+    if (error.response && error.response.status === 401) {
+      const refreshToken = user.accessToken;
+
+      if (refreshToken) {      
+        removeUserFromLocalStorage();
+        navigate("/sign-in"); 
+      } else {        
+        removeUserFromLocalStorage(); 
+        navigate("/sign-in"); 
+      }
     }
-    return config
-})
-export default customFetch
+    return Promise.reject(error);
+  }
+);
+
+export default customFetch;
+
+
+// import axios from "axios";
+// import { getUserLocalStorage } from "../store/localStorage";
+
+//  const customFetch = axios.create({
+//     baseURL: "https://educrat-be.onrender.com/api/v1"
+// })
+
+// customFetch.interceptors.request.use((config)=>{
+//     const user = getUserLocalStorage();
+//     if(user){
+//         config.headers["Authorization"] = `Bearer ${user.accessToken}`
+//     }
+//     return config
+// })
+// export default customFetch
