513dd5d83256d010ff6a599ae944422f98936053
Adedokun peter <peteradedokun436@gmail.com>
2024-04-14 00:18:14 +0100

    update

--- src/hooks/studentCourse/index.tsx
+++ src/hooks/studentCourse/index.tsx
@@ -72,7 +72,9 @@ export const useGetAllUserEnrolledCourse = () => {
 };
 //
 export const useGetAlInstructorPublishedCourse = (id: any) => {
-  const { data: getAlInstructorPublishedCourse } = useQuery({
+  const toast = useToast();
+  const [errorToastShown, setErrorToastShown] = useState(false);
+  const { data: getAlInstructorPublishedCourse, isError } = useQuery({
     queryKey: ["getAllInstructorPublishCourse", id],
     queryFn: async ({ queryKey }) => {
       const [, id] = queryKey;
@@ -80,7 +82,44 @@ export const useGetAlInstructorPublishedCourse = (id: any) => {
       return data;
     },
   });
-
+  useEffect(() => {
+    if (isError && !errorToastShown) {
+      setErrorToastShown(true);
+      toast({
+        title: "Error fetching data",
+        status: "error",
+        position: "bottom-right",
+        duration: null,
+        isClosable: false,
+        render: ({ onClose }) => (
+          <Stack bg={"#FCBCA0"} py={3} px={4}>
+            <Flex align={"center"} columnGap={2}>
+              <Text>
+                <TbInfoHexagonFilled size={30} />
+              </Text>
+              <Text fontWeight={"bold"}>Network Error</Text>
+            </Flex>
+            <Flex columnGap={3} mt={4}>
+              <Text
+                as={"button"}
+                fontWeight={"bold"}
+                onClick={() => window.location.reload()}
+                color={"white"}
+                py={1}
+                px={4}
+                backgroundColor={"black"}
+              >
+                Reload page
+              </Text>
+              <Text as={"button"} fontWeight={"bold"} onClick={onClose}>
+                Dismiss
+              </Text>
+            </Flex>
+          </Stack>
+        ),
+      });
+    }
+  }, [isError, errorToastShown, toast]);
   return { getAlInstructorPublishedCourse };
 };
 
