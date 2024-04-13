d11ef9bf32464e567056a0ddbe2414219a1c842d
Adedokun peter <peteradedokun436@gmail.com>
2024-04-14 00:14:58 +0100

    bug fixes

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,10 +1,14 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
 import customFetch from "../utils/axios";
-import { useToast } from "@chakra-ui/react";
+import { Flex, Stack, Text, useToast } from "@chakra-ui/react";
 
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { TbInfoHexagonFilled } from "react-icons/tb";
 
 export const useGetUser = () => {
+  const toast = useToast();
+  const [errorToastShown, setErrorToastShown] = useState(false);
   const { data, isPending, isError } = useQuery({
     queryKey: ["user"],
     queryFn: async () => {
@@ -12,6 +16,44 @@ export const useGetUser = () => {
       return data;
     },
   });
+  useEffect(() => {
+    if (isError && !errorToastShown) {
+      setErrorToastShown(true);
+      toast({
+        title: "Error fetching data",
+        status: "error",
+        position: "bottom-right",
+        duration: null,
+        isClosable: false,
+        render: ({ onClose }) => (
+          <Stack bg={"#FCBCA0"} py={3} px={4}>
+            <Flex align={"center"} columnGap={2}>
+              <Text>
+                <TbInfoHexagonFilled size={30} />
+              </Text>
+              <Text fontWeight={"bold"}>Network Error</Text>
+            </Flex>
+            <Flex columnGap={3} mt={4}>
+              <Text
+                as={"button"}
+                fontWeight={"bold"}
+                onClick={() => window.location.reload()}
+                color={"white"}
+                py={1}
+                px={4}
+                backgroundColor={"black"}
+              >
+                Reload page
+              </Text>
+              <Text as={"button"} fontWeight={"bold"} onClick={onClose}>
+                Dismiss
+              </Text>
+            </Flex>
+          </Stack>
+        ),
+      });
+    }
+  }, [isError, errorToastShown, toast]);
   return { data, isPending, isError };
 };
 
