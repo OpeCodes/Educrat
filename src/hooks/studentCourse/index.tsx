2db8bf560bc183243b7aca213bef6edfbfb4f754
Adedokun peter <peteradedokun436@gmail.com>
2024-04-14 00:42:14 +0100

    update

--- src/hooks/studentCourse/index.tsx
+++ src/hooks/studentCourse/index.tsx
@@ -178,7 +178,6 @@ export const useCourseEnrollment = () => {
     },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["getCourseEnroll"] });
-
       toast({
         title: `course enrollment successfull`,
         status: "success",
@@ -215,7 +214,9 @@ export const useCourseEnrollment = () => {
 };
 
 export const useGetSingleEnrolledStudentCourse = (id: any) => {
-  const { data: getSingleEnrolledCourse, isPending } = useQuery({
+  const toast = useToast();
+  const [errorToastShown, setErrorToastShown] = useState(false);
+  const { data: getSingleEnrolledCourse, isPending, isError } = useQuery({
     queryKey: ["getCourseStudentEnrollCourse", id],
     queryFn: async ({ queryKey }) => {
       const [, id] = queryKey;
@@ -223,6 +224,44 @@ export const useGetSingleEnrolledStudentCourse = (id: any) => {
       return data;
     },
   });
+  useEffect(() => {
+    if (isError && !errorToastShown) {
+      setErrorToastShown(true);
+      toast({
+        title: "Error fetching data",
+        status: "error",
+        position: "bottom-right",
+        duration: null,
+        isClosable: false,
+        render: ({ onClose }) => (
+          <Stack bg={"#FCBCA0"} py={3} px={4}>
+            <Flex align={"center"} columnGap={2}>
+              <Text>
+                <TbInfoHexagonFilled size={30} />
+              </Text>
+              <Text fontWeight={"bold"}>Network Error</Text>
+            </Flex>
+            <Flex columnGap={3} mt={4}>
+              <Text
+                as={"button"}
+                fontWeight={"bold"}
+                onClick={() => window.location.reload()}
+                color={"white"}
+                py={1}
+                px={4}
+                backgroundColor={"black"}
+              >
+                Reload page
+              </Text>
+              <Text as={"button"} fontWeight={"bold"} onClick={onClose}>
+                Dismiss
+              </Text>
+            </Flex>
+          </Stack>
+        ),
+      });
+    }
+  }, [isError, errorToastShown, toast]);
   return {
     getSingleEnrolledCourse,
     isPending,
