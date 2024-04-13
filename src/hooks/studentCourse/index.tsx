b555f1668c4af10527ba70a526607dafe7144cee
Adedokun peter <peteradedokun436@gmail.com>
2024-04-14 00:44:03 +0100

    update

--- src/hooks/studentCourse/index.tsx
+++ src/hooks/studentCourse/index.tsx
@@ -332,7 +332,13 @@ export const useGetSingleEnrolledCourse = (id: any) => {
 };
 
 export const useGetStudentEnrolledCourse = (id: any) => {
-  const { data: getStudentEnrolledCourse, isPending } = useQuery({
+  const toast = useToast();
+  const [errorToastShown, setErrorToastShown] = useState(false);
+  const {
+    data: getStudentEnrolledCourse,
+    isPending,
+    isError,
+  } = useQuery({
     queryKey: ["getCourseReview111", id],
     queryFn: async ({ queryKey }) => {
       const [, id] = queryKey;
@@ -342,6 +348,44 @@ export const useGetStudentEnrolledCourse = (id: any) => {
       return data;
     },
   });
+  useEffect(() => {
+    if (isError && !errorToastShown) {
+      setErrorToastShown(true);
+      toast({
+        title: "Error fetching data",
+        status: "error",
+        position: "bottom-right",
+        duration: null,
+        isClosable: false,
+        render: ({ onClose }) => (
+          <Stack bg={"#FCBCA0"} py={3} px={4}>
+            <Flex align={"center"} columnGap={2}>
+              <Text>
+                <TbInfoHexagonFilled size={30} />
+              </Text>
+              <Text fontWeight={"bold"}>Network Error</Text>
+            </Flex>
+            <Flex columnGap={3} mt={4}>
+              <Text
+                as={"button"}
+                fontWeight={"bold"}
+                onClick={() => window.location.reload()}
+                color={"white"}
+                py={1}
+                px={4}
+                backgroundColor={"black"}
+              >
+                Reload page
+              </Text>
+              <Text as={"button"} fontWeight={"bold"} onClick={onClose}>
+                Dismiss
+              </Text>
+            </Flex>
+          </Stack>
+        ),
+      });
+    }
+  }, [isError, errorToastShown, toast]);
   return {
     getStudentEnrolledCourse,
     isPending,
