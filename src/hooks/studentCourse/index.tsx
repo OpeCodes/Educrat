f89d9187b4e2ec35f7e9ca0ac820895843c260c2
Adedokun peter <peteradedokun436@gmail.com>
2024-04-15 00:11:24 +0100

    update

--- src/hooks/studentCourse/index.tsx
+++ src/hooks/studentCourse/index.tsx
@@ -219,7 +219,7 @@ export const useCourseEnrollment = () => {
 
 export const useGetSingleEnrolledStudentCourse = (id: any) => {
   const toast = useToast();
-  const [errorToastShown, setErrorToastShown] = useState(false);
+  const [error, setError] = useState<string | null>(null);
   const {
     data: getSingleEnrolledCourse,
     isPending,
@@ -228,48 +228,49 @@ export const useGetSingleEnrolledStudentCourse = (id: any) => {
     queryKey: ["getCourseStudentEnrollCourse", id],
     queryFn: async ({ queryKey }) => {
       const [, id] = queryKey;
-      const { data } = await customFetch.get(`/enrollment/${id}`);
-      return data;
+      try {
+       const { data } = await customFetch.get(`/enrollment/${id}`);
+        setError(null);
+        return data;
+      } catch (error: any) {
+        if (
+          error.response &&
+          error.response.data &&
+          error.response.data.error
+        ) {
+          setError(error.response.data.error);
+        } else {
+          setError("An unexpected error occurred.");
+        }
+        throw error;
+      }
     },
   });
   useEffect(() => {
-    if (isError && !errorToastShown) {
-      setErrorToastShown(true);
-      toast({
-        title: "Error fetching data",
-        status: "error",
-        position: "bottom-right",
-        duration: 5000,
-        isClosable: false,
-        render: ({ onClose }) => (
-          <Stack bg={"#FCBCA0"} py={3} px={4}>
-            <Flex align={"center"} columnGap={2}>
-              <Text>
-                <TbInfoHexagonFilled size={30} />
-              </Text>
-              <Text fontWeight={"bold"}>Network Error</Text>
-            </Flex>
-            <Flex columnGap={3} mt={4}>
-              <Text
-                as={"button"}
-                fontWeight={"bold"}
-                onClick={() => window.location.reload()}
-                color={"white"}
-                py={1}
-                px={4}
-                backgroundColor={"black"}
-              >
-                Reload page
-              </Text>
-              <Text as={"button"} fontWeight={"bold"} onClick={onClose}>
-                Dismiss
-              </Text>
-            </Flex>
-          </Stack>
-        ),
-      });
+    if (isError) {
+      if (error) {
+        toast({
+          status: "error",
+          position: "bottom-right",
+          duration: 10000,
+          isClosable: true,
+          render: ({ onClose }) => (
+            <GetToastErrorHandling error={error} onClose={onClose} />
+          ),
+        });
+      } else {
+        toast({
+          status: "error",
+          position: "bottom-right",
+          duration: 10000,
+          isClosable: true,
+          render: ({ onClose }) => (
+            <GetToastErrorHandling error={"Network Error"} onClose={onClose} />
+          ),
+        });
+      }
     }
-  }, [isError, errorToastShown, toast]);
+  }, [isError, error, toast]);
   return {
     getSingleEnrolledCourse,
     isPending,
