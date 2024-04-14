0f9ed74b8d1839b13c17e48c88821034a98eb1b0
Adedokun peter <peteradedokun436@gmail.com>
2024-04-15 00:13:48 +0100

    update

--- src/hooks/studentCourse/index.tsx
+++ src/hooks/studentCourse/index.tsx
@@ -229,7 +229,7 @@ export const useGetSingleEnrolledStudentCourse = (id: any) => {
     queryFn: async ({ queryKey }) => {
       const [, id] = queryKey;
       try {
-       const { data } = await customFetch.get(`/enrollment/${id}`);
+        const { data } = await customFetch.get(`/enrollment/${id}`);
         setError(null);
         return data;
       } catch (error: any) {
@@ -399,7 +399,6 @@ export const useGetStudentEnrolledCourse = (id: any) => {
 
 export const useGetInstructorenrolledCourse = (id: any) => {
   const toast = useToast();
-  const [errorToastShown, setErrorToastShown] = useState(false);
   const {
     data: getInstructorenrolledCourse,
     isPending,
@@ -414,44 +413,7 @@ export const useGetInstructorenrolledCourse = (id: any) => {
       return data;
     },
   });
-  useEffect(() => {
-    if (isError && !errorToastShown) {
-      setErrorToastShown(true);
-      toast({
-        title: "Error fetching data",
-        status: "error",
-        position: "bottom-right",
-        duration: 5000,
-        isClosable: false,
-        render: ({ onClose }) => (
-          <Stack bg={"#FCBCA0"} py={3} px={4}>
-            <Flex align={"center"} columnGap={2}>
-              <Text>
-                <TbInfoHexagonFilled size={30} />
-              </Text>
-              <Text fontWeight={"bold"}>Network Error</Text>
-            </Flex>
-            <Flex columnGap={3} mt={4}>
-              <Text
-                as={"button"}
-                fontWeight={"bold"}
-                onClick={() => window.location.reload()}
-                color={"white"}
-                py={1}
-                px={4}
-                backgroundColor={"black"}
-              >
-                Reload page
-              </Text>
-              <Text as={"button"} fontWeight={"bold"} onClick={onClose}>
-                Dismiss
-              </Text>
-            </Flex>
-          </Stack>
-        ),
-      });
-    }
-  }, [isError, errorToastShown, toast]);
+
   return {
     getInstructorenrolledCourse,
     isPending,
