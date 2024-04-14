94d4758837558ce50da7964d493009c8dfa86a1c
Adedokun peter <peteradedokun436@gmail.com>
2024-04-15 00:15:01 +0100

    update

--- src/hooks/studentCourse/index.tsx
+++ src/hooks/studentCourse/index.tsx
@@ -116,9 +116,7 @@ export const useGetAllUserEnrolledCourse = () => {
 };
 //
 export const useGetAlInstructorPublishedCourse = (id: any) => {
-  const toast = useToast();
-  const [errorToastShown, setErrorToastShown] = useState(false);
-  const { data: getAlInstructorPublishedCourse, isError } = useQuery({
+  const { data: getAlInstructorPublishedCourse,  } = useQuery({
     queryKey: ["getAllInstructorPublishCourse", id],
     queryFn: async ({ queryKey }) => {
       const [, id] = queryKey;
@@ -126,44 +124,6 @@ export const useGetAlInstructorPublishedCourse = (id: any) => {
       return data;
     },
   });
-  useEffect(() => {
-    if (isError && !errorToastShown) {
-      setErrorToastShown(true);
-      toast({
-        title: "Error fetching data",
-        status: "error",
-        position: "bottom-right",
-        duration: 5000,
-        isClosable: false,
-        render: ({ onClose }) => (
-          <Stack bg={"#FCBCA0"} py={3} px={4}>
-            <Flex align={"center"} columnGap={2}>
-              <Text>
-                <TbInfoHexagonFilled size={30} />
-              </Text>
-              <Text fontWeight={"bold"}>Network Error</Text>
-            </Flex>
-            <Flex columnGap={3} mt={4}>
-              <Text
-                as={"button"}
-                fontWeight={"bold"}
-                onClick={() => window.location.reload()}
-                color={"white"}
-                py={1}
-                px={4}
-                backgroundColor={"black"}
-              >
-                Reload page
-              </Text>
-              <Text as={"button"} fontWeight={"bold"} onClick={onClose}>
-                Dismiss
-              </Text>
-            </Flex>
-          </Stack>
-        ),
-      });
-    }
-  }, [isError, errorToastShown, toast]);
   return { getAlInstructorPublishedCourse };
 };
 
@@ -398,11 +358,9 @@ export const useGetStudentEnrolledCourse = (id: any) => {
 };
 
 export const useGetInstructorenrolledCourse = (id: any) => {
-  const toast = useToast();
   const {
     data: getInstructorenrolledCourse,
     isPending,
-    isError,
   } = useQuery({
     queryKey: ["getInstructorCourseReview", id],
     queryFn: async ({ queryKey }) => {
