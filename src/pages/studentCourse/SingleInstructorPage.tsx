9bc71c2ecf9f94efa6222f7bf75a7843f63d0763
Adedokun peter <peteradedokun436@gmail.com>
2024-04-16 15:24:35 +0100

    single instructor page course student layout

--- src/pages/studentCourse/SingleInstructorPage.tsx
+++ src/pages/studentCourse/SingleInstructorPage.tsx
@@ -9,6 +9,10 @@ import {
   Tab,
   TabPanel,
   TabIndicator,
+  Grid,
+  GridItem,
+  Image,
+  Divider,
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { AiFillStar } from "react-icons/ai";
@@ -19,7 +23,8 @@ import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
 import { TiSocialTwitter } from "react-icons/ti";
 import { FaYoutube } from "react-icons/fa";
 import { TbWorld } from "react-icons/tb";
-
+import { CgNotes } from "react-icons/cg";
+const dummy = [1, 2, 4, 5, 6, 8];
 const SingleInstructorPage = () => {
   return (
     <Stack mt={"4.3rem"}>
@@ -51,7 +56,7 @@ const SingleInstructorPage = () => {
         mt={4}
         width={"100%"}
         bg={"#6440fb"}
-        maxW={"70%"}
+        maxW={{ md: "70%" }}
         mx={"auto"}
         borderRadius={5}
         py={"5rem"}
@@ -118,7 +123,12 @@ const SingleInstructorPage = () => {
           </Text>
         </Flex>
       </Stack>
-      <Stack width={"100%"} mt={12} maxW={"60%"} mx={"auto"}>
+      <Stack
+        width={"100%"}
+        mt={12}
+        maxW={{ base: "95%", md: "60%" }}
+        mx={"auto"}
+      >
         <Tabs position="relative">
           <TabList fontWeight={"bold"}>
             <Tab _selected={{ color: "#6440fb" }}>Overview </Tab>
@@ -135,9 +145,59 @@ const SingleInstructorPage = () => {
           <TabPanels>
             <TabPanel>1</TabPanel>
             <TabPanel>
-              <Stack>
-                <Text>2</Text>
-              </Stack>
+              <Grid
+                templateColumns={{
+                  base: "repeat(1, 1fr)",
+                  md: "repeat(2, 1fr)",
+                }}
+                gap={6}
+              >
+                {dummy.map((_, index) => (
+                  <GridItem w="100%" key={index}>
+                    <Stack>
+                      <Stack>
+                        <Image
+                          src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                          alt="Green double couch with wooden legs"
+                          borderRadius="lg"
+                        />
+                        <Stack>
+                          <Text>4.3 rating</Text>
+                          <Text fontSize="20px" mt="-12px">
+                            learn node js
+                          </Text>
+                          <Flex justify={"space-between"}>
+                            <Flex align="center" columnGap={"4px"} color="gray">
+                              <CgNotes />
+                              <Text fontSize="13px">6 Lessons</Text>
+                            </Flex>
+                            <Flex align="center" columnGap={"4px"} color="gray">
+                              <CgNotes />
+                              <Text fontSize="13px">6 Lessons</Text>
+                            </Flex>
+                            <Flex align="center" columnGap={"4px"} color="gray">
+                              <CgNotes />
+                              <Text fontSize="13px">6 Lessons</Text>
+                            </Flex>
+                          </Flex>
+                          <Divider />
+                        </Stack>
+                      </Stack>
+                      <Flex align={"center"} justify={"space-between"}>
+                        <Flex align={"center"} columnGap={2}>
+                          <Avatar name="Dan Abrahmov" size={"sm"} />
+                          <Text>Peter Adedokun</Text>
+                        </Flex>
+                        <Text fontWeight={"500"} fontSize={"20px"}>
+                          $30
+                        </Text>
+                      </Flex>
+                    </Stack>
+                  </GridItem>
+                ))}
+
+                <GridItem w="100%" h="10" bg="blue.500" />
+              </Grid>
             </TabPanel>
           </TabPanels>
         </Tabs>
