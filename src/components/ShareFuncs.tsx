f9edfce399b87cde0b29754b42a7961e890a993f
Adedokun peter <peteradedokun436@gmail.com>
2024-04-16 16:38:18 +0100

    shareOnInstagram func

--- src/components/ShareFuncs.tsx
+++ src/components/ShareFuncs.tsx
@@ -49,3 +49,9 @@ export const shareOnFacebook = (url: string) => {
     const shareUrl = `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(url)}`;
     window.open(shareUrl, '_blank');
   };
+   export const shareOnInstagram = (url:  string) => {
+    const caption = encodeURIComponent(`Check out this link: ${url}`);
+    const shareUrl = `https://www.instagram.com/create/caption/?caption=${caption}`;
+    window.open(shareUrl, '_blank');
+  };
+  
