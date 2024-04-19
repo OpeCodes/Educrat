f8f1e8ef6fd71d4b983cea6e9faf73bb6e6bae63
Adedokun peter <peteradedokun436@gmail.com>
2024-04-19 18:54:04 +0100

    footer display

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -10,12 +10,13 @@ import {
   Stack,
   Flex,
   Image,
+  Button,
 } from "@chakra-ui/react";
 import { IoCartOutline } from "react-icons/io5";
 import { MdClose } from "react-icons/md";
 
 const AddToCartButton = () => {
-  const array = [1, 2,];
+  const array = [1, 2];
   return (
     <>
       <Popover placement="bottom-start">
@@ -52,33 +53,63 @@ const AddToCartButton = () => {
           width={"400px"}
         >
           <PopoverBody>
-            <Stack  maxH={"250px"} h={"100%"} overflow={"auto"}     borderWidth={0} >
-            {array.map(() => {
-              return (
-                <Stack px={1} py={3}>
-                  <Flex justify={"space-between"} columnGap={2}>
-                    <Flex columnGap={2}>
-                      <Image
-                        boxSize="70px"
-                        objectFit="cover"
-                        src={"https://bit.ly/dan-abramov"}
-                      />
-                      <Stack>
-                        <Text width={"90%"} fontSize={16}>
-                          NodeJS Tutorial and Projects Course
-                        </Text>
-                        <Text>$50</Text>
-                      </Stack>
+            <Stack maxH={"250px"} h={"100%"} overflow={"auto"} borderWidth={0}>
+              {array.map(() => {
+                return (
+                  <Stack px={1} py={3}>
+                    <Flex justify={"space-between"} columnGap={2}>
+                      <Flex columnGap={2}>
+                        <Image
+                          boxSize="70px"
+                          objectFit="cover"
+                          src={"https://bit.ly/dan-abramov"}
+                        />
+                        <Stack>
+                          <Text width={"90%"} fontSize={16}>
+                            NodeJS Tutorial and Projects Course
+                          </Text>
+                          <Text>$50</Text>
+                        </Stack>
+                      </Flex>
+                      <Text
+                        cursor={"pointer"}
+                        color={"blue"}
+                        fontWeight={"bold"}
+                      >
+                        <MdClose fontSize={20} />
+                      </Text>
                     </Flex>
-                    <Text cursor={"pointer"} color={"blue"} fontWeight={"bold"}><MdClose fontSize={20}/></Text>
-                  </Flex>
-                </Stack>
-              );
-
-            })}
+                  </Stack>
+                );
+              })}
             </Stack>
           </PopoverBody>
-          <PopoverFooter>This is the footer</PopoverFooter>
+          <PopoverFooter>
+            <Stack mx={3}>
+              <Flex justify={"space-between"} my={2}>
+                <Text>Total:</Text>
+                <Text>$168</Text>
+              </Flex>
+              <Flex justify={"center"} columnGap={10}>
+                <Button
+                  bg={"#6440FB"}
+                  py={"25px"}
+                  variant="solid"
+                  color={"white"}
+                >
+                  Go to Course
+                </Button>
+                <Button
+                  bg={"#6440FB"}
+                  py={"25px"}
+                  variant="solid"
+                  color={"white"}
+                >
+                  Checkout
+                </Button>
+              </Flex>
+            </Stack>
+          </PopoverFooter>
         </PopoverContent>
       </Popover>
     </>
