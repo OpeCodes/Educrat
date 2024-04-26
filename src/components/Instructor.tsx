3723bc571116fbb9dfbfe919ba267b2fe3ff357f
Adedokun peter <peteradedokun436@gmail.com>
2024-04-26 14:20:31 +0100

    update

--- src/components/Instructor.tsx
+++ src/components/Instructor.tsx
@@ -5,7 +5,7 @@ import {
   FaFacebookF,
   FaLinkedinIn,
   FaStar,
-  FaYoutube,ins
+  FaYoutube,
 } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useInstructorReviewRating } from "../hooks/studentCourse";
