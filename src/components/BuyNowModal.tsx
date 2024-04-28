39c3919128ba9a371438504aec4eb32b42fe4084
Adedokun peter <peteradedokun436@gmail.com>
2024-04-28 17:56:24 +0100

    modal design

--- src/components/BuyNowModal.tsx
+++ src/components/BuyNowModal.tsx
@@ -4,7 +4,6 @@ import { RootState } from "../store/store";
 import { setToggleOrder } from "../features/user/UserSlice";
 import { useDispatch } from "react-redux";
 
-
 const BuyNowModal = () => {
   const { orderBoolean } = useSelector((store: RootState) => store?.user);
   const dispatch = useDispatch();
@@ -18,26 +17,52 @@ const BuyNowModal = () => {
           position="fixed"
           top={0}
           left={0}
-          width="100%"
           height="100%"
+          width={"100%"}
           backgroundColor="rgba(0, 0, 0, 0.5)"
           justifyContent="center"
           alignItems="center"
           zIndex={999}
         >
-          <Flex
-            backgroundColor="white"
-            borderRadius="md"
-            flexDirection="column"
-            padding={6}
-            maxWidth="400px"
-            boxShadow="lg"
+          <Stack
+            bg={"white"}
+            rowGap={"5"}
+            py={"4rem"}
+            color={"black"}
+            width={"500px"}
+            height={"100px"}
+            direction={"column"}
+            align={"center"}
+            justify={"center"}
           >
-            hi
-            <Button onClick={closeModal} marginTop={4}>
-              Close
-            </Button>
-          </Flex>
+            <Stack>
+              <Text fontSize={20} fontWeight={"bold"}>
+                Will you like to proceed to purchase this course?
+              </Text>
+            </Stack>
+            <Flex
+              borderRadius="md"
+              columnGap={4}
+            >
+              <Button
+                onClick={closeModal}
+                width={"100%"}
+                bg={"black"}
+                color={"white"}
+              >
+                No
+              </Button>
+
+              <Button
+                onClick={closeModal}
+                width={"100%"}
+                bg={"black"}
+                color={"white"}
+              >
+                Yes
+              </Button>
+            </Flex>
+          </Stack>
         </Flex>
       )}
     </>
