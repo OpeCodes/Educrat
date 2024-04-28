1a04d654f9c814fced3d76d988089c8395d537f2
Adedokun peter <peteradedokun436@gmail.com>
2024-04-28 19:47:55 +0100

    update

--- src/pages/instructor/courses/CreateCourse.tsx
+++ src/pages/instructor/courses/CreateCourse.tsx
@@ -79,19 +79,7 @@ const CreateCourse = () => {
                 name="category"
                 onChange={handleChange}
                 mt={6}
-                value={values.category}
-              >
-                {data?.map((values: any) => (
-                  <option key={values.id} id={values.id} value={values.id}>
-                    {values.name}
-                  </option>
-                ))}
-              </Select>
-              <Select
-                placeholder="Select Category"
-                name="category"
-                onChange={handleChange}
-                mt={6}
+                variant="filled"
                 value={values.category}
               >
                 {data?.map((values: any) => (
