3c44157fbe600a8e5387ed4091bea24753ca9f7b
Adedokun peter <peteradedokun436@gmail.com>
2024-04-30 19:50:23 +0100

    checkout page aside section

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  Divider,
   Flex,
   Grid,
   GridItem,
@@ -7,19 +8,21 @@ import {
   Stack,
   Text,
 } from "@chakra-ui/react";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
 
 const Checkout = () => {
+  const { courses } = useSelector((store: RootState) => store?.cart);
+
   return (
     <Stack mt={"5rem"}>
       <Grid templateColumns={{ lg: "repeat(2, 1fr)" }} columnGap={5}>
         <GridItem
           height="100vh"
-          // mx={{ base: "15px", lg: "20px" }}
           justifyContent={"center"}
           alignContent={"center"}
           display={"flex"}
           mt={"2rem"}
-          // maxWeight={"50%"}
         >
           <Stack>
             <Text fontSize={"2rem"} fontWeight={"500"}>
@@ -29,24 +32,33 @@ const Checkout = () => {
               Billing address
             </Text>
             <Stack>
-              <Text fontSize={"1.8rem"} fontWeight={"500"}>
+              <Text fontSize={"1.5rem"} fontWeight={"700"}>
                 Order details
               </Text>
-
-              <Flex columnGap={10} justify={"space-between"} align={"center"}>
-                <Flex columnGap={3} align={"center"}>
-                  <Image
-                    boxSize="45px"
-                    objectFit="cover"
-                    src="https://bit.ly/dan-abramov"
-                    alt="Dan Abramov"
-                  />
-                  <Text fontWeight={"bold"}>
-                    HTML&CSS Tutorial and Projects Course (Flexbox&Grid)
-                  </Text>
-                </Flex>
-                <Text>100</Text>
-              </Flex>
+              {courses?.map((course: any) => {
+                return (
+                  <Flex
+                    mt={"rem"}
+                    columnGap={10}
+                    justify={"space-between"}
+                    align={"center"}
+                  >
+                    <Flex columnGap={2} align={"center"}>
+                      <Image
+                        boxSize="40px"
+                        objectFit="cover"
+                        src={course.img}
+                        alt="Dan Abramov"
+                      />
+                      <Text fontWeight={"bold"}>
+                        HTML&CSS Tutorial and Projects Course (Flexbox&Grid)
+                        {course.title}
+                      </Text>
+                    </Flex>
+                    <Text>100</Text>
+                  </Flex>
+                );
+              })}
             </Stack>
           </Stack>
         </GridItem>
@@ -55,11 +67,29 @@ const Checkout = () => {
             boxSize="sm"
             w="50%"
             h="100vh"
-            bg={"#140342"}
+            bg={"#f7f9fa"}
             mx={"auto"}
             display={{ base: "none", lg: "block" }}
             position={"fixed"}
-          ></Box>
+          >
+            <Text fontWeight={"bold"}>Summary</Text>
+            <Stack>
+              <Flex>
+                <Text>Original Price</Text>
+                <Text>N1000</Text>
+              </Flex>
+              <Flex>
+                <Text>Original Price</Text>
+                <Text>N1000</Text>
+              </Flex>
+
+              <Divider />
+              <Flex fontWeight={"bold"}>
+                <Text>Original Price</Text>
+                <Text>N1000</Text>
+              </Flex>
+            </Stack>
+          </Box>
         </GridItem>
       </Grid>
     </Stack>
