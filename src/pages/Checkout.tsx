a736068a52b703cefc23b42efbbaebfc48541ad2
Adedokun peter <peteradedokun436@gmail.com>
2024-04-30 20:37:16 +0100

    update

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -6,15 +6,21 @@ import {
   Grid,
   GridItem,
   Image,
+  Radio,
+  RadioGroup,
   Select,
   Stack,
   Text,
 } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 import { RootState } from "../store/store";
+import { useState } from "react";
+import paystack from "../assets/paystack.png";
 
 const Checkout = () => {
   const { courses } = useSelector((store: RootState) => store?.cart);
+
+  const [value, setValue] = useState("false");
 
   return (
     <Stack mt={"5rem"}>
@@ -26,14 +32,14 @@ const Checkout = () => {
           display={"flex"}
           mt={"2rem"}
         >
-          <Stack>
+          <Stack maxW={"70%"} w={"full"}>
             <Text fontSize={"2rem"} fontWeight={"500"}>
               Checkout
             </Text>
             <Text fontSize={"1.5rem"} fontWeight={"500"}>
               Billing address
             </Text>
-            <Stack mr={"20rem"} my={"0.5rem"}>
+            <Stack my={"0.5rem"}>
               <Flex justify={"space-between"} align={"center"}>
                 <Text fontWeight={"bold"}>Country</Text>
                 <Text color={"gray"} fontSize={12}>
@@ -43,6 +49,28 @@ const Checkout = () => {
               <Select>
                 <option>Nigeria</option>
               </Select>
+            </Stack>
+            <Stack>
+              <Flex align={"center"} justify={"space-between"}>
+                <Text fontSize={"1.5rem"} fontWeight={"500"}>
+                  Payment Method
+                </Text>
+
+                <Stack>
+                  <Text color={"gray"} fontSize={12}>
+                    Secured connection
+                  </Text>
+                </Stack>
+              </Flex>
+              <Stack borderWidth={1} p={2} bg={"#F7F9FA"} cursor={"pointer"}>
+                <RadioGroup onChange={setValue} value={value}>
+                  <Stack direction="row">
+                    <Radio value={"true"}>
+                      <Image src={paystack} width={"110px"} />
+                    </Radio>
+                  </Stack>
+                </RadioGroup>
+              </Stack>
             </Stack>
             <Stack>
               <Text fontSize={"1.5rem"} fontWeight={"700"}>
