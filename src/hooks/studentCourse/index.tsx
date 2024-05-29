f359a4fd6f78072ea22e5e6e2c5380ab32fa250a
Adedokun peter <peteradedokun436@gmail.com>
2024-05-29 14:57:12 +0100

    update

--- src/hooks/studentCourse/index.tsx
+++ src/hooks/studentCourse/index.tsx
@@ -1,8 +1,7 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import customFetch from "../../utils/axios";
-import { Flex, Stack, Text, useToast } from "@chakra-ui/react";
+import {  useToast } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import { TbInfoHexagonFilled } from "react-icons/tb";
 import { GetToastErrorHandling } from "../../components";
 
 export const useGetAllEducratInstructors = () => {
@@ -394,8 +393,6 @@ export const useGetInstructorReview = (id: any) => {
 // ************************************wishlist*****************************
 
 export const useGetStudentWishList = () => {
-  const toast = useToast();
-  const [errorToastShown, setErrorToastShown] = useState(false);
   const {
     data: getStudentWishList,
     isError,
@@ -408,44 +405,7 @@ export const useGetStudentWishList = () => {
       return data;
     },
   });
-  useEffect(() => {
-    if (isError && !errorToastShown) {
-      setErrorToastShown(true);
-      toast({
-        title: "Error fetching data",
-        status: "error",
-        position: "bottom-right",
-        duration: 5000,
-        isClosable: false,
-        render: ({ onClose }) => (
-          <Stack bg={"#FCBCA0"} py={3} px={4}>
-            <Flex align={"center"} columnGap={2}>
-              <Text>
-                <TbInfoHexagonFilled size={30} />
-              </Text>
-              <Text fontWeight={"bold"}>Network Error</Text>
-            </Flex>
-            <Flex columnGap={3} mt={4}>
-              <Text
-                as={"button"}
-                fontWeight={"bold"}
-                onClick={() => window.location.reload()}
-                color={"white"}
-                py={1}
-                px={4}
-                backgroundColor={"black"}
-              >
-                Reload page
-              </Text>
-              <Text as={"button"} fontWeight={"bold"} onClick={onClose}>
-                Dismiss
-              </Text>
-            </Flex>
-          </Stack>
-        ),
-      });
-    }
-  }, [isError, errorToastShown, toast]);
+  
   return { getStudentWishList, isPending, isError, refetch };
 };
 
